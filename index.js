6f47575857847624a770822cd87c807eb3ccc7e7
Camden Withrow <cmwithrow@anderson.edu>
2023-04-19 12:32:57 -0400

    js + correct add bank location

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const addBudgetButton = document.getElementById("add-budget");
+const addHoursButton = document.getElementById("add-hours");
+
+addBudgetButton.addEventListener("click", handleClickAddBank("budget"))
+addHoursButton.addEventListener("click", handleClickAddBank("hours"))
+
+const handleClickAddBank = (bankType) => {
+
+};
+
+/**
+ * Handle  storing and adding new bank to user dash from user input
+ * @param {string} bankType Which bank category to add to
+ */
+const addBank = (bankType) => {
+  //
+};
